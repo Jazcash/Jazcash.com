8e32f09e4f5307acbc2603b43e88d711bba79be2
jazcash <jaspercashmore@gmail.com>
2016-04-03 21:29:10 +0100

    Really log commit message for testing on Jazcash.com. Multiple lines! More lines! Wheeeeeeeeeeeeeeeeeeeeeeeeeeee

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 var express = require('express'),
   config = require('./config/config');
 
@@ -8,6 +6,5 @@ var app = express();
 require('./config/express')(app, config);
 
 app.listen(config.port, function () {
-  console.log('Express server listening on port ' + config.port);
+	console.log('Express server listening on port ' + config.port);
 });
-
